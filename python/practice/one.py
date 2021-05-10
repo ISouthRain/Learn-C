4ff6b7ec54d52b6ea4421007dbaa6c26082836e9
ISouthRain <gaozhongjie2@gmail.com>
2021-05-10 20:27:44 +0800

    登上顶点吧!

--- python/practice/one.py
+++ python/practice/one.py
@@ -4,3 +4,15 @@ print(str[5:2:-1])
 sites = {'Google', 'Taobao', 'Runoob', 'Facebook', 'Zhihu', 'Baidu','Baidu'}
 
 print(sites)   # 输出集合，重复的元素被自动去掉
+a=100
+print(a<<2)
+x = True
+y = False
+z = False
+if not y:
+    print('1')
+i=[1,2]
+if i in range(1,50):
+    print('jack')
+import math
+print(math.acos(0.3))
