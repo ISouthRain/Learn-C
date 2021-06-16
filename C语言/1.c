2a18c850d361ba93c644ba086dabd20128c93498
ISouthRain <gaozhongjie2@gmail.com>
2021-06-16 19:02:34 +0800

    登上顶点吧!

---
+++ C语言/1.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main()
+{
+    printf("Jack");
+    printf("Woai");
+    printf("jack");
+}
--- python/main.py
+++
@@ -1,6 +0,0 @@
-from bs4 import BeautifulSoup
-
-with open('https://www.runoob.com/python3/python3-iterator-generator.html','r') as html_file:
-    content = html_file.read()
-    soup = BeautifulSoup (content,'lxml')
-    tags = soup.find_all('h5')
