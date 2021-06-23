fd993cd0bb3bca115d520735229cf927ddaf080e
ISouthRain <gaozhongjie2@gmail.com>
2021-06-23 14:13:41 +0800

    登上顶点吧!

--- python/1.py
+++ python/1.py
@@ -1,4 +1,1 @@
-import math
-a = str(input("请输入你想转换的字符："))
-print("jack""\n""lal")
-print("{0} {1} {2} {3}\n{4}\n{5} {6} {7} {8}\n{9}     {10}\n{11} {12} {13} {14}".format(a,a,a,a,a,a,a,a,a,a,a,a,a,a,a))
+print('jack')
