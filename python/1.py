b48fcfb05875990c4f0e2ff27b4679a9f6c20102
ISouthRain <gaozhongjie2@gmail.com>
2021-07-08 17:16:58 +0800

    登上顶点吧！

--- python/1.py
+++ python/1.py
@@ -1,17 +1,1 @@
-import math
-input('我爱你')
-print(math.log(10,10))
-l=[1]
-t1=(1,2,3)
-t2=(4,5,6)
-t3=t1+t2
-print(t3)
-dict = {'Name': t3, 'Age': t3, '罗杰': 'First'}
-del dict['Name']
-print(dict)
-print(dict.keys(),dict.values(),dict.items(),len(dict))
-print('罗杰')
-if t3==t1:
-    print('agp')
-print('罗杰')
-print('罗杰')
+
